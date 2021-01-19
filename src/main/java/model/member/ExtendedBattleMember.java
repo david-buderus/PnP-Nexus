f567b1afebca2561a8241e2a06dd31145ded9973
David Buderus <dbuderus@mail.uni-paderborn.de>
2021-01-19 14:36:45 +0100

    Better distribution between different types of jewellery Add belt and other support for jewellery

--- src/main/java/model/member/ExtendedBattleMember.java
+++ src/main/java/model/member/ExtendedBattleMember.java
@@ -222,14 +222,26 @@ public class ExtendedBattleMember extends BattleMember {
         this.jewellery = FXCollections.observableArrayList();
         if (usesJewellery()) {
 
+            Map<String, Integer> amountPerType = new HashMap<>();
+            int maxAmount = 0;
+
             for (String type : config.getStringArray("character.jewellery.types")) {
                 int amount = config.getInt("character.jewellery.amount." + type);
                 String localizedType = LanguageUtility.hasMessage("jewellery.type." + type) ?
                         LanguageUtility.getMessage("jewellery.type." + type) : type;
 
-                for (int i = 0; i < amount; i++) {
+                amountPerType.put(localizedType, amount);
+                maxAmount += amount;
+            }
+
+            for (int i = 0; i < maxAmount; i++) {
+                Optional<String> opType = amountPerType.keySet().stream().skip(random.nextInt(amountPerType.keySet().size())).findFirst();
+
+                if (opType.isPresent()) {
                     if (random.nextDouble() < getTier() / 100f) {
-                        this.jewellery.add((Jewellery) randomJewellery(localizedType, jewelleryPool).getWithUpgrade());
+                        String type = opType.get();
+                        this.jewellery.add((Jewellery) randomJewellery(type, jewelleryPool).getWithUpgrade());
+                        amountPerType.put(type, amountPerType.get(type) - 1);
                     }
                 }
             }
