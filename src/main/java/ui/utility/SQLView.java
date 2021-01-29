2935570b37bb4ed6d1d0d2b29fc3f6c388a0e12c
David Buderus <dbuderus@mail.uni-paderborn.de>
2021-01-29 16:33:40 +0100

    Better system in SQLView

--- src/main/java/ui/utility/SQLView.java
+++ src/main/java/ui/utility/SQLView.java
@@ -1,31 +1,27 @@
 package ui.utility;
 
 import javafx.beans.property.ListProperty;
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import manager.Database;
 import manager.LanguageUtility;
 import model.Spell;
-import model.item.Armor;
-import model.item.Item;
-import model.item.Jewellery;
-import model.item.Weapon;
+import model.item.*;
 import model.upgrade.UpgradeModel;
 import ui.IView;
 import ui.ViewPart;
 import ui.part.WrappingTableCell;
 
-import java.beans.IntrospectionException;
-import java.beans.PropertyDescriptor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -37,45 +33,92 @@ public class SQLView extends ViewPart {
         TabPane root = new TabPane();
         root.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
 
-        String[] labels = new String[]{"column.name", "column.type", "column.item.subtype", "column.tier", "column.item.rarity", "column.item.price", "column.effect"};
-        String[] names = new String[]{"name", "type", "subtype", "tier", "rarity", "currency", "effect"};
+        List<ColumnDefinition<Item>> itemColumns = Arrays.asList(
+                new ColumnDefinition<>("column.name", Item::getName),
+                new ColumnDefinition<>("column.type", Item::getTier),
+                new ColumnDefinition<>("column.item.subtype", Item::getSubtype),
+                new ColumnDefinition<>("column.tier", Item::getTier),
+                new ColumnDefinition<>("column.item.rarity", Item::getRarity),
+                new ColumnDefinition<>("column.item.price", Item::getCurrency),
+                new ColumnDefinition<>("column.item.effect", Item::getEffect)
+        );
+        root.getTabs().add(createTab("sql.tab.items", itemColumns, Database.itemList));
 
-        root.getTabs().add(createTab("sql.tab.items", labels, names, Database.itemList, Item.class));
+        List<ColumnDefinition<Weapon>> weaponColumns = Arrays.asList(
+                new ColumnDefinition<>("column.name", Item::getName),
+                new ColumnDefinition<>("column.type", Item::getSubtype),
+                new ColumnDefinition<>("column.weapon.initiative", Weapon::getInitiative),
+                new ColumnDefinition<>("column.dice_weight", Weapon::getDice),
+                new ColumnDefinition<>("column.damage_protection", Weapon::getDamage),
+                new ColumnDefinition<>("column.weapon.hit", Weapon::getHit),
+                new ColumnDefinition<>("column.tier", Item::getTier),
+                new ColumnDefinition<>("column.item.rarity", Item::getRarity),
+                new ColumnDefinition<>("column.item.price", Item::getCurrency),
+                new ColumnDefinition<>("column.effect", Item::getEffect),
+                new ColumnDefinition<>("column.equipment.slots", Equipment::getUpgradeSlots),
+                new ColumnDefinition<>("column.equipment.requirement", Equipment::getRequirement)
+        );
+        root.getTabs().add(createTab("sql.tab.weapons", weaponColumns, Database.weaponList));
 
-        labels = new String[]{"column.name", "column.type", "column.weapon.initiative", "column.dice_weight", "column.damage_protection", "column.weapon.hit", "column.tier", "column.item.rarity", "column.item.price", "column.effect", "column.equipment.slots", "column.equipment.requirement"};
-        names = new String[]{"name", "subtype", "initiative", "dice", "damage", "hit", "tier", "rarity", "currency", "effect", "upgradeSlots", "requirement"};
+        List<ColumnDefinition<Armor>> armorColumns = Arrays.asList(
+                new ColumnDefinition<>("column.name", Item::getName),
+                new ColumnDefinition<>("column.type", Item::getSubtype),
+                new ColumnDefinition<>("column.armor.protection", Armor::getProtection),
+                new ColumnDefinition<>("column.armor.weight", Armor::getWeight),
+                new ColumnDefinition<>("column.tier", Item::getTier),
+                new ColumnDefinition<>("column.item.rarity", Item::getRarity),
+                new ColumnDefinition<>("column.item.price", Item::getCurrency),
+                new ColumnDefinition<>("column.effect", Item::getEffect),
+                new ColumnDefinition<>("column.equipment.slots", Equipment::getUpgradeSlots),
+                new ColumnDefinition<>("column.equipment.requirement", Equipment::getRequirement)
+        );
+        root.getTabs().add(createTab("sql.tab.armor", armorColumns, Database.armorList));
 
-        root.getTabs().add(createTab("sql.tab.weapons", labels, names, Database.weaponList, Weapon.class));
+        List<ColumnDefinition<Jewellery>> jewelleryColumns = Arrays.asList(
+                new ColumnDefinition<>("column.name", Item::getName),
+                new ColumnDefinition<>("column.type", Item::getSubtype),
+                new ColumnDefinition<>("column.equipment.material", Equipment::getMaterial),
+                new ColumnDefinition<>("column.jewellery.gem", Jewellery::getGem),
+                new ColumnDefinition<>("column.tier", Item::getTier),
+                new ColumnDefinition<>("column.item.rarity", Item::getRarity),
+                new ColumnDefinition<>("column.item.price", Item::getCurrency),
+                new ColumnDefinition<>("column.effect", Item::getEffect),
+                new ColumnDefinition<>("column.equipment.slots", Equipment::getUpgradeSlots),
+                new ColumnDefinition<>("column.equipment.requirement", Equipment::getRequirement)
+        );
+        root.getTabs().add(createTab("sql.tab.jewellery", jewelleryColumns, Database.jewelleryList));
 
-        labels = new String[]{"column.name", "column.type", "Schutz", "Belastung", "column.tier", "column.item.rarity", "column.item.price", "column.effect", "column.equipment.slots", "column.equipment.requirement"};
-        names = new String[]{"name", "subtype", "protection", "weight", "tier", "rarity", "currency", "effect", "upgradeSlots", "requirement"};
+        List<ColumnDefinition<Spell>> spellColumns = Arrays.asList(
+                new ColumnDefinition<>("column.name", Spell::getName),
+                new ColumnDefinition<>("column.effect", Spell::getEffect),
+                new ColumnDefinition<>("column.type", Spell::getType),
+                new ColumnDefinition<>("column.spell.cost", Spell::getCost),
+                new ColumnDefinition<>("column.spell.castTime", Spell::getCastTime),
+                new ColumnDefinition<>("column.tier", Spell::getTier)
+        );
+        root.getTabs().add(createTab("sql.tab.spell", spellColumns, Database.spellList));
 
-        root.getTabs().add(createTab("sql.tab.armor", labels, names, Database.armorList, Armor.class));
-
-        labels = new String[]{"column.name", "column.type", "Material", "Edelstein", "column.tier", "column.item.rarity", "column.item.price", "column.effect", "column.equipment.slots", "column.equipment.requirement"};
-        names = new String[]{"name", "subtype", "material", "gem", "tier", "rarity", "currency", "effect", "upgradeSlots", "requirement"};
-
-        root.getTabs().add(createTab("sql.tab.jewellery", labels, names, Database.jewelleryList, Jewellery.class));
-
-        labels = new String[]{"column.name", "column.effect", "column.type", "column.spell.cost", "column.spell.castTime", "column.tier"};
-        names = new String[]{"name", "effect", "type", "cost", "castTime", "tier"};
-
-        root.getTabs().add(createTab("sql.tab.spell", labels, names, Database.spellList, Spell.class));
-
-        labels = new String[]{"column.name", "Stufe", "Ziel", "column.effect", "column.equipment.slots", "column.item.price", "column.upgrade.mana", "column.upgrade.materials"};
-        names = new String[]{"name", "level", "target", "effect", "slots", "cost", "mana", "materials"};
-
-        root.getTabs().add(createTab("sql.tab.upgrades", labels, names, Database.upgradeModelList, UpgradeModel.class));
+        List<ColumnDefinition<UpgradeModel>> upgradeColumns = Arrays.asList(
+                new ColumnDefinition<>("column.name", UpgradeModel::getName),
+                new ColumnDefinition<>("column.upgrade.level", UpgradeModel::getLevel),
+                new ColumnDefinition<>("column.target", UpgradeModel::getTarget),
+                new ColumnDefinition<>("column.effect", UpgradeModel::getEffect),
+                new ColumnDefinition<>("column.equipment.slots", UpgradeModel::getSlots),
+                new ColumnDefinition<>("column.item.price", UpgradeModel::getCost),
+                new ColumnDefinition<>("column.upgrade.mana", UpgradeModel::getMana),
+                new ColumnDefinition<>("column.upgrade.materials", UpgradeModel::getMaterials)
+        );
+        root.getTabs().add(createTab("sql.tab.upgrades", upgradeColumns, Database.upgradeModelList));
 
 
         this.setContent(root);
     }
 
-    private Tab createTab(String key, String[] labels, String[] names, ListProperty<?> utility, Class<?> itemClass) {
+    private <Obj> Tab createTab(String key, List<ColumnDefinition<Obj>> columnDefinitions, ListProperty<Obj> utility) {
         Tab tab = new Tab();
         tab.textProperty().bind(LanguageUtility.getMessageProperty(key));
 
-        ListProperty<Object> list = new SimpleListProperty<>();
+        ListProperty<Obj> list = new SimpleListProperty<>();
         utility.addListener((ob, o, n) -> list.set(n.stream().collect(Collectors.toCollection(FXCollections::observableArrayList))));
         list.set(utility.stream().collect(Collectors.toCollection(FXCollections::observableArrayList)));
 
@@ -83,31 +126,25 @@ public class SQLView extends ViewPart {
         vBox.setPadding(new Insets(10, 20, 20, 20));
         tab.setContent(vBox);
 
-        TableView<Object> table = new TableView<>();
+        TableView<Obj> table = new TableView<>();
         table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         table.itemsProperty().bindBidirectional(list);
         table.itemsProperty().addListener((ob, o, n) -> table.layout());
         vBox.getChildren().add(table);
 
-        ArrayList<FilterContainer> filterContainers = new ArrayList<>();
+        ArrayList<FilterContainer<Obj>> filterContainers = new ArrayList<>();
 
         HBox input = new HBox();
         vBox.getChildren().add(input);
 
-        for (int i = 0; i < labels.length; i++) {
-            TableColumn<Object, Object> column = new TableColumn<>();
-            column.textProperty().bind(LanguageUtility.getMessageProperty(labels[i]));
+        for (ColumnDefinition<Obj> columnDefinition : columnDefinitions) {
+
+            TableColumn<Obj, String> column = new TableColumn<>();
+            column.textProperty().bind(LanguageUtility.getMessageProperty(columnDefinition.key));
             column.setMaxWidth(400);
-            column.setCellValueFactory(new PropertyValueFactory<>(names[i]));
+            column.setCellValueFactory(cell -> new ReadOnlyStringWrapper(String.valueOf(columnDefinition.getter.apply(cell.getValue()))));
             column.setCellFactory(col -> new WrappingTableCell<>());
             table.getColumns().add(column);
-
-            Method getter = null;
-            try {
-                getter = new PropertyDescriptor(names[i], itemClass).getReadMethod();
-            } catch (IntrospectionException e) {
-                e.printStackTrace();
-            }
 
             TextField textField = new TextField();
             textField.minWidthProperty().bind(column.widthProperty());
@@ -115,10 +152,7 @@ public class SQLView extends ViewPart {
             textField.textProperty().addListener((ob, o, n) -> update(utility, list, filterContainers));
             input.getChildren().add(textField);
 
-            FilterContainer container = new FilterContainer();
-            container.textField = textField;
-            container.method = getter;
-            filterContainers.add(container);
+            filterContainers.add(new FilterContainer<>(textField, columnDefinition.getter));
         }
 
         tab.selectedProperty().addListener((ob, o, n) -> getStage().sizeToScene());
@@ -126,25 +160,33 @@ public class SQLView extends ViewPart {
         return tab;
     }
 
-    private void update(Collection<?> utility, ListProperty<Object> list, Collection<FilterContainer> filterContainers) {
-        Stream<?> stream = utility.stream();
+    private <Obj> void update(Collection<Obj> utility, ListProperty<Obj> list, Collection<FilterContainer<Obj>> filterContainers) {
+        Stream<Obj> stream = utility.stream();
 
-        for (FilterContainer container : filterContainers) {
-            stream = stream.filter(x -> {
-                try {
-                    return (String.valueOf(container.method.invoke(x))).toLowerCase().contains(container.textField.getText().toLowerCase());
-                } catch (IllegalAccessException | InvocationTargetException e) {
-                    e.printStackTrace();
-                }
-                return false;
-            });
+        for (FilterContainer<Obj> container : filterContainers) {
+            stream = stream.filter(x -> String.valueOf(container.getter.apply(x)).toLowerCase().contains(container.textField.getText().toLowerCase()));
         }
 
         list.set(stream.collect(Collectors.toCollection(FXCollections::observableArrayList)));
     }
 
-    private static class FilterContainer {
-        private TextField textField;
-        private Method method;
+    private static class ColumnDefinition<Obj> {
+        private final String key;
+        private final Function<Obj, Object> getter;
+
+        public ColumnDefinition(String key, Function<Obj, Object> getter) {
+            this.key = key;
+            this.getter = getter;
+        }
+    }
+
+    private static class FilterContainer<Obj> {
+        private final TextField textField;
+        private final Function<Obj, Object> getter;
+
+        public FilterContainer(TextField textField, Function<Obj, Object> getter) {
+            this.textField = textField;
+            this.getter = getter;
+        }
     }
 }
