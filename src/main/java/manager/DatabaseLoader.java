51bbc29046fc37a736431ed50e3ccf3abacd386e
Bastian Haverkamp <bastihav@mail.upb.de>
2021-09-08 11:25:19 +0200

    add print

--- src/main/java/manager/DatabaseLoader.java
+++ src/main/java/manager/DatabaseLoader.java
@@ -74,6 +74,7 @@ public abstract class DatabaseLoader {
                 checkInconsistencies();
 
             } catch (InterruptedException ignored) {
+                ignored.printStackTrace();
             }
         }
 
