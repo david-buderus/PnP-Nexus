3dbcc6a356a0b6b636064c438366c5b5bc35c321
Bastian Haverkamp <bastihav@mail.upb.de>
2021-09-13 10:44:18 +0200

    add session overview draft

--- app/src/main/java/de/pnp/manager/app/SessionOverview.kt
+++ app/src/main/java/de/pnp/manager/app/SessionOverview.kt
@@ -11,22 +11,22 @@ import androidx.navigation.fragment.findNavController
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class LoginFragment : Fragment() {
+class SessionOverview : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_sessions, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_connect_to_server).setOnClickListener {
-            findNavController().navigate(R.id.action_LoginFragment_to_SessionOverviewFragment)
-        }
+        /*view.findViewById<Button>(R.id.button_connect_to_server).setOnClickListener {
+            findNavController().navigate(R.id.action_SessionOverviewFragment_to_CharacterFragment)
+        } */
 
     }
 }
