b8859b2a26ead4ac0123f8734cc75e751a7de7ff
Bastian Haverkamp <{ID}+{username}@users.noreply.github.com>
2023-06-17 15:42:49 +0200

    Add basic navbar concept

---
+++ webapp/src/lib/NavbarEntry.svelte
@@ -0,0 +1,16 @@
+<a class="flex" href={destination}>
+    {#if icon !== undefined}
+        <Fa class="pl-2 py-2" size="3x" icon={icon} />
+    {/if}
+    <p class="font-bold ml-4 py-2">{heading}</p>
+</a>
+  
+<script lang="ts">
+	import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+	import Fa from "svelte-fa";
+
+    export let heading :string;
+    export let icon: IconDefinition | undefined = undefined;
+    export let destination: string;
+    
+</script>
--- webapp/src/routes/+layout.svelte
+++ webapp/src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
+	import Navbar from "$lib/Navbar.svelte";
   import "../app.css";
+
+  export const ssr = false;
 </script>
 
+<Navbar/>
 <slot />
