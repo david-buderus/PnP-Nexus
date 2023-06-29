e1396cd1f45389cd02007334a6ff5ef045b59013
david-buderus <buderus.david@gmx.de>
2023-06-29 21:41:47 +0200

    Start

--- server/src/main/java/de/pnp/manager/server/ServerApplication.java
+++ server/src/main/java/de/pnp/manager/server/ServerApplication.java
@@ -2,11 +2,24 @@ package de.pnp.manager.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ServerApplication {
+public class ServerApplication implements WebMvcConfigurer {
 
   public static void main(String[] args) {
     SpringApplication.run(ServerApplication.class, args);
   }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("**.html");
+    }
 }
