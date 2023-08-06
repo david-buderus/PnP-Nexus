b73772be0c8b2b628f73d249583390007c97f01a
david-buderus <buderus.david@gmx.de>
2023-08-06 13:28:08 +0200

    Added MatchingItemTypesValidator

--- server/src/main/java/de/pnp/manager/ServerApplication.java
+++ server/src/main/java/de/pnp/manager/ServerApplication.java
@@ -4,7 +4,11 @@ import de.pnp.manager.server.EJvmFlag;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -39,5 +43,28 @@ public class ServerApplication implements WebMvcConfigurer {
                 registry.addMapping("/**").allowedMethods("*");
             }
         }
+    }
+
+    /**
+     * Source for message resolution.
+     * <p>
+     * This enables the use of messages.properties for internationalization.
+     */
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    /**
+     * Link the {@link #messageSource()} with the validators.
+     */
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
     }
 }
