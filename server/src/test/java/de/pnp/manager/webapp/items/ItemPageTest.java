834657aa75e3bdf702cd7c5a3808e10cd780fcaf
david-buderus <buderus.david@gmx.de>
2024-04-20 20:09:19 +0200

    Testing

--- server/src/test/java/de/pnp/manager/webapp/items/ItemPageTest.java
+++ server/src/test/java/de/pnp/manager/webapp/items/ItemPageTest.java
@@ -1,10 +1,15 @@
 package de.pnp.manager.webapp.items;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.TimeoutError;
 import de.pnp.manager.component.item.ERarity;
 import de.pnp.manager.component.item.Item;
 import de.pnp.manager.webapp.pages.ItemPage;
 import de.pnp.manager.webapp.pages.MainMenu;
 import de.pnp.manager.webapp.pages.components.ItemCreation.EItemClass;
+import java.nio.file.Path;
 import java.util.Collection;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
@@ -16,7 +21,14 @@ public class ItemPageTest extends ItemPageTestBase {
 
     @Override
     protected ItemPage openTestPage(MainMenu mainMenu) {
-        return mainMenu.openItemPage();
+        try {
+            return mainMenu.openItemPage();
+        } catch (TimeoutError e) {
+            final Path path = Path.of("build", "reports", "tests", "test", "screenshot.png");
+            mainMenu.asPage().screenshot(new Page.ScreenshotOptions()
+                .setPath(path));
+            return fail(e);
+        }
     }
 
     @Override
