180ca9470d323e96cb6346c3faeef1ac9333b3e2
david-buderus <buderus.david@gmx.de>
2024-05-02 10:18:38 +0200

    Adjust CI

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: '../server/src/main/resources/static',
+    sourcemap: 'inline',
     rollupOptions: {
       output: {
         manualChunks(id) {
