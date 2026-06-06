c6b80ddfbd6d7674ec87298bfcb071d845dbdd39
David Buderus <buderus.david@gmx.de>
2026-06-06 13:37:35 +0200

    Added context menu

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -5,6 +5,7 @@ import Login from './pages/login';
 import '@mantine/core/styles.css';
 import '@mantine/tiptap/styles.css';
 import '@mantine/notifications/styles.css';
+import 'mantine-contextmenu/styles.layer.css';
 
 import {MantineProvider} from '@mantine/core';
 import Home from './pages/home';
@@ -35,6 +36,8 @@ import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {QueryCache} from '@tanstack/query-core';
 import {handleNetworkErrors} from './components/utils/ErrorUtils';
 import {Notifications} from '@mantine/notifications';
+import {ModalsProvider} from '@mantine/modals';
+import {ContextMenuProvider} from 'mantine-contextmenu';
 
 const QUERY_CLIENT = new QueryClient({
     queryCache: new QueryCache({
@@ -47,64 +50,72 @@ class App extends Component {
     override render() {
         return (
             <MantineProvider defaultColorScheme="auto">
-                <Notifications/>
-                <QueryClientProvider client={QUERY_CLIENT}>
-                    <Router>
-                        <style>{`
-                    body {
-                        margin: 0px;
-                        padding: 0px;
-                    }
-                `}
-                        </style>
-                        <Routes>
-                            <Route path="/" element={
-                                <QueryParamProvider adapter={ReactRouter6Adapter}>
-                                    <PageBase/>
-                                </QueryParamProvider>
-                            }>
-                                <Route path="/" element={<Home/>}></Route>
-                                <Route path="/user" element={<User/>}> </Route>
-                                <Route path="/preferences" element={<UserPreferences/>}/>
-                                <Route path="/universe" element={<UniverseView><UniverseOverview/></UniverseView>}/>
-                                <Route path="/universe-creation" element={<UniverseCreation/>}/>
-                                <Route path="/about"/>
-                                <Route path="/items" element={<UniverseView><Items/></UniverseView>}/>
-                                <Route path="/weapons" element={<UniverseView><Weapons/></UniverseView>}/>
-                                <Route path="/shields" element={<UniverseView><Shields/></UniverseView>}/>
-                                <Route path="/armor" element={<UniverseView><ArmorOverview/></UniverseView>}/>
-                                <Route path="/jewellery"
-                                       element={<UniverseView><JewelleryOverview/></UniverseView>}/>
-                                <Route path="/upgrades"
-                                       element={<UniverseView><UpgradeOverview/></UniverseView>}/>
-                                <Route path="/materials"
-                                       element={<UniverseView><MaterialOverview/></UniverseView>}/>
-                                <Route path="/crafting-recipes"
-                                       element={<UniverseView><CraftingRecipeOverview/></UniverseView>}/>
-                                <Route path="/upgrade-recipes"
-                                       element={<UniverseView><UpgradeRecipeOverview/></UniverseView>}/>
-                                <Route path="/characters"
-                                       element={<UniverseView><CharactersOverview/> </UniverseView>}/>
-                                <Route path="/characters/:character"
-                                       element={<UniverseView><CharacterEditor/> </UniverseView>}/>
-                                <Route path="/characters-editor"
-                                       element={<UniverseView><CharacterSheetsOverview/></UniverseView>}/>
-                                <Route path="/characters-editor/:sheet"
-                                       element={<UniverseView><CharacterSheetEditor/></UniverseView>}/>
-                                <Route path="/spells" element={<UniverseView><SpellOverview/></UniverseView>}/>
-                                <Route path="/talents" element={<UniverseView><TalentOverview/></UniverseView>}/>
-                                <Route path="/species" element={<UniverseView><SpeciesOverview/></UniverseView>}/>
-                                <Route path="/species/:species"
-                                       element={<UniverseView><SpeciesDetail/></UniverseView>}/>
-                                <Route path="/nations" element={<UniverseView><NationView/></UniverseView>}/>
-                                <Route path="/nations/:nation" element={<UniverseView><NationView/></UniverseView>}/>
-                                <Route path="/admin" element={<Admin/>}/>
-                                <Route path="/users" element={<UserOverview/>}/>
-                            </Route>
-                            <Route path="/login" element={<Login/>}> </Route>
-                        </Routes>
-                    </Router>
-                </QueryClientProvider>
+                <ModalsProvider>
+                    <ContextMenuProvider>
+                        <Notifications/>
+                        <QueryClientProvider client={QUERY_CLIENT}>
+                            <Router>
+                                <style>{`
+                                            body {
+                                                margin: 0px;
+                                                padding: 0px;
+                                            }
+                                        `}
+                                </style>
+                                <Routes>
+                                    <Route path="/" element={
+                                        <QueryParamProvider adapter={ReactRouter6Adapter}>
+                                            <PageBase/>
+                                        </QueryParamProvider>
+                                    }>
+                                        <Route path="/" element={<Home/>}></Route>
+                                        <Route path="/user" element={<User/>}> </Route>
+                                        <Route path="/preferences" element={<UserPreferences/>}/>
+                                        <Route path="/universe"
+                                               element={<UniverseView><UniverseOverview/></UniverseView>}/>
+                                        <Route path="/universe-creation" element={<UniverseCreation/>}/>
+                                        <Route path="/about"/>
+                                        <Route path="/items" element={<UniverseView><Items/></UniverseView>}/>
+                                        <Route path="/weapons" element={<UniverseView><Weapons/></UniverseView>}/>
+                                        <Route path="/shields" element={<UniverseView><Shields/></UniverseView>}/>
+                                        <Route path="/armor" element={<UniverseView><ArmorOverview/></UniverseView>}/>
+                                        <Route path="/jewellery"
+                                               element={<UniverseView><JewelleryOverview/></UniverseView>}/>
+                                        <Route path="/upgrades"
+                                               element={<UniverseView><UpgradeOverview/></UniverseView>}/>
+                                        <Route path="/materials"
+                                               element={<UniverseView><MaterialOverview/></UniverseView>}/>
+                                        <Route path="/crafting-recipes"
+                                               element={<UniverseView><CraftingRecipeOverview/></UniverseView>}/>
+                                        <Route path="/upgrade-recipes"
+                                               element={<UniverseView><UpgradeRecipeOverview/></UniverseView>}/>
+                                        <Route path="/characters"
+                                               element={<UniverseView><CharactersOverview/> </UniverseView>}/>
+                                        <Route path="/characters/:character"
+                                               element={<UniverseView><CharacterEditor/> </UniverseView>}/>
+                                        <Route path="/characters-editor"
+                                               element={<UniverseView><CharacterSheetsOverview/></UniverseView>}/>
+                                        <Route path="/characters-editor/:sheet"
+                                               element={<UniverseView><CharacterSheetEditor/></UniverseView>}/>
+                                        <Route path="/spells" element={<UniverseView><SpellOverview/></UniverseView>}/>
+                                        <Route path="/talents"
+                                               element={<UniverseView><TalentOverview/></UniverseView>}/>
+                                        <Route path="/species"
+                                               element={<UniverseView><SpeciesOverview/></UniverseView>}/>
+                                        <Route path="/species/:species"
+                                               element={<UniverseView><SpeciesDetail/></UniverseView>}/>
+                                        <Route path="/nations" element={<UniverseView><NationView/></UniverseView>}/>
+                                        <Route path="/nations/:nation"
+                                               element={<UniverseView><NationView/></UniverseView>}/>
+                                        <Route path="/admin" element={<Admin/>}/>
+                                        <Route path="/users" element={<UserOverview/>}/>
+                                    </Route>
+                                    <Route path="/login" element={<Login/>}> </Route>
+                                </Routes>
+                            </Router>
+                        </QueryClientProvider>
+                    </ContextMenuProvider>
+                </ModalsProvider>
             </MantineProvider>
         );
     }
